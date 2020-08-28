ec7a7e34c4d25f931aef9ad715d0ba342381ac96
caueguedes <caue.guedes91@gmail.com>
2020-08-28 19:13:14 -0300

    Database - Question and QuestionAccesses entities generated

---
+++ db/migrate/20200827022331_create_question_accesses.rb
@@ -0,0 +1,11 @@
+class CreateQuestionAccesses < ActiveRecord::Migration[6.0]
+  def change
+    create_table :question_accesses do |t|
+      t.datetime :date
+      t.integer :times_accessed
+
+      t.timestamps
+    end
+    add_reference :question_accesses, :question, foreign_key: true
+  end
+end
