82dc60a818439a1851dc5e18ecb634f4a209e60b
caueguedes <caue.guedes91@gmail.com>
2020-09-01 21:59:15 -0300

    configuration - Kaminari and paginatable object added

---
+++ app/controllers/concerns/pagination.rb
@@ -0,0 +1,15 @@
+module Pagination
+  extend ActiveSupport::Concern
+
+  def set_paginate_params
+    @page = params[:current_page] || 1
+    @per_page = params[:per_page] || 10
+  end
+
+  private
+    def paginate(relation)
+      collection = relation.page(@page).per(@per_page)
+      raise ActiveRecord::RecordNotFound.new('No Records Found') if collection.empty?
+      collection
+    end
+end
