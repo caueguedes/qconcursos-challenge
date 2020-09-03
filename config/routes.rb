e38fcd9cce8b76545ff00118ef7743eacdbae607
caueguedes <caue.guedes91@gmail.com>
2020-09-03 01:33:24 -0300

    Routes path and expectations

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,3 @@
 Rails.application.routes.draw do
-  resources :questions, only: [:index, :show] do
-    collection do
-      get '/list/:filter', to: 'questions#list', as: 'list'
-    end
-  end
+  resources :questions, only: [:index, :show]
 end
--- spec/routing/questions_routing_spec.rb
+++ spec/routing/questions_routing_spec.rb
@@ -7,19 +7,7 @@ RSpec.describe QuestionsController, type: :routing do
     end
 
     it "routes to #show" do
-      expect(:get => "/questions/1").to route_to("questions#show", :id => "1")
-    end
-
-    it "routes to #list" do
-      expect(:get => "/questions/list/week").to route_to("questions#list", :filter => "week")
-    end
-
-    it "routes to #list" do
-      expect(:get => "/questions/list/month").to route_to("questions#list", :filter => 'month')
-    end
-
-    it "routes to #list" do
-      expect(:get => "/questions/list/year").to route_to("questions#list", :filter => 'year')
+      expect(:get => "/questions/1").to route_to("questions#show", :id => '1')
     end
   end
 end
