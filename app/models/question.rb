2a315c0646f888c701ca4fbe9d4e024c8f43bf38
caueguedes <caue.guedes91@gmail.com>
2020-09-03 01:49:40 -0300

    Filterable concern - to easy filtering

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,5 +1,8 @@
 class Question < ApplicationRecord
   has_many :question_accesses
 
+  scope :filter_by_discipline, -> (discipline) { where(discipline: discipline) }
+
   include QuestionValidator
+  include Filterable
 end
