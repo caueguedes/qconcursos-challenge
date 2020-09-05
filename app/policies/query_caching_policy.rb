9ce4993cb00a9a461773492aadb04d0669558d84
caueguedes <caue.guedes91@gmail.com>
2020-09-05 00:40:38 -0300

    QueryCachingPolicy - politica para geração de cache para query

---
+++ app/policies/query_caching_policy.rb
@@ -0,0 +1,22 @@
+class QueryCachingPolicy
+  MAXIMUM_PAGES = 10
+  ALLOWED_PARAMS = %w( discipline period page)
+  CACHING_TIME = 2.minutes
+
+  def initialize(params:)
+    @params = params
+  end
+
+  def caching_allowed?
+    allowed_params? && page_number_allowed?
+  end
+
+  private
+    def allowed_params?
+      @params[:page] <= MAXIMUM_PAGES
+    end
+
+    def page_number_allowed?
+      @params.keys.all? { |key| ALLOWED_PARAMS.include?(key) }
+    end
+end
